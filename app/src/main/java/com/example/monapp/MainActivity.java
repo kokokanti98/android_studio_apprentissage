f3a37ae430bbffcee6c2404e401f6a38632cfae8
KANTY <KANTY@DESKTOP-5KBC3FN>
2021-01-27 12:16:20 +0100

    AS User Interface basic first commit

--- app/src/main/java/com/example/monapp/MainActivity.java
+++ app/src/main/java/com/example/monapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.monapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onHelloBtnCicked(View view) {
+
+        //Selection de design Textview
+        TextView TxtMessage = findViewById(R.id.txtWelcome);
+        //Changer le text dans TextView
+        TxtMessage.setText("Hi everyone");
+
     }
 }
