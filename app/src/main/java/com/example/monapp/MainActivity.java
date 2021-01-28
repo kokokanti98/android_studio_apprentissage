62293b47749160179aeba7ec0d011455bfafaf9b
KANTY <KANTY@DESKTOP-5KBC3FN>
2021-01-28 11:58:06 +0100

    AS User Interface basic  Different fichier XML first commit

--- app/src/main/java/com/example/monapp/MainActivity.java
+++ app/src/main/java/com/example/monapp/MainActivity.java
@@ -7,16 +7,27 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ListView CitiesList;
+
+    private TextView TxtHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Selection
+        TxtHello = findViewById(R.id.txtHello);
+
+        //le string hello est sur /src/main/res/values/strings.xml
+        //clic droit sur values->new->valuesRessources files->local->ex:german
+        //le fichier doit avoir le meme nom ici c'est strings.xml
+        TxtHello.setText(getString(R.string.hello));
+
     }
 }
