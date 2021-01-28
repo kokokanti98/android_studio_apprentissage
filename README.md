227ec4c0307c463d1991a0feab5e8ede341ec901
KANTY <KANTY@DESKTOP-5KBC3FN>
2021-01-28 10:13:55 +0100

    AS User Interface basic RY first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # android_studio_apprentissage
 <h1>Apprendre Android Studio</h1>
 <h4>Initialisation</h4>
-<h4>Initialisation d'un projet android studio avec hello au lieu de Hello World!</h4>
+<h4>Initialisation à Android Studio RelativeLayouts</h4>
--- app/src/main/java/com/example/monapp/MainActivity.java
+++ app/src/main/java/com/example/monapp/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.monapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView CitiesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
